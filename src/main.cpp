7a59e9034bdd0c866e7a2c32b91c0bfa0a866035
Rafal Magda <rafalmag@gmail.com>
2018-05-08 01:52:30 +0200

    typo fix

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,12 @@
 // http://www.winsen-sensor.com/d/files/PDF/Infrared%20Gas%20Sensor/NDIR%20CO2%20SENSOR/MH-Z14%20CO2%20V2.4.pdf
 
 // connections:
-HardwareSerial serialCo2(2); // RX(green) to GPIO 16, TX(blue) to GPIO 17
-                             // analog (White) to GPIO 36 aka ADC1_CHANNEL_0
-#define PWM_CO2 12           // pwm (yellow) to GPIO 12
-#define LED 5                // on board LED for Lolin32
+HardwareSerial hwSerial(2); // RX(green) to GPIO 16, TX(blue) to GPIO 17
+                            // analog (White) to GPIO 36 aka ADC1_CHANNEL_0
+#define PWM_CO2 12          // pwm (yellow) to GPIO 12
+#define LED 5               // on board LED for Lolin32
 
-Co2FromSerial co2FromSerial(serialCo2);
+Co2FromSerial co2FromSerial(hwSerial);
 Co2FromAdc co2FromAdc;
 Co2FromPwm co2FromPwm(PWM_CO2);
 
@@ -50,7 +50,7 @@ void setup()
 
 void loop()
 {
-  int serialCO2 = co2FromSerial.getCO2();
+  int serialCo2 = co2FromSerial.getCO2();
   int adcCo2 = co2FromAdc.getCO2();
   int pwmCo2 = co2FromPwm.getCO2();
 
@@ -59,7 +59,8 @@ void loop()
   Serial.println(String("pwm   : ") + pwmCo2);
   Serial.println();
 
-  lcd.setCursor(0, 0);
+  lcd.clear();
+  // lcd.setCursor(0, 0);
   lcd.print(String("serial: ") + serialCo2);
   lcd.setCursor(0, 1);
   lcd.print(String("analog: ") + adcCo2);
